015e2554c567aeafba423418eb6c21703dab2069
Jamie Pilgrim <jamieleepilgrim@gmail.com>
2015-05-27 11:22:07 -0500

    put in temp link to email, contact form needs'a'fixin

--- app.rb
+++ app.rb
@@ -16,7 +16,26 @@ post '/contact' do
   name = params[:name]
   mail = params[:mail]
   body = params[:body]
-  Pony.mail(:to => 'hello@jamiepilgrim.com', :from => "#{mail}", :subject => "Website Inquiry from #{name}", :body => "#{body}" )
+  # Pony.mail(:to => 'hello@jamiepilgrim.com', :via => :sendmail, :from => "#{mail}", :subject => "Website Inquiry from #{name}", :body => "#{body}" )
+
+  Pony.mail(
+      :from => params[:name],
+      :to => 'jamieleepilgrim@gmail.com',
+      :subject => params[:name] + " has contacted you",
+      :body => params[:message],
+      :port => '587',
+      :via => :smtp,
+      :via_options => { 
+        :address              => 'smtp.gmail.com', 
+        :port                 => '465', 
+        :enable_starttls_auto => true, 
+        :user_name            => 'jamieleepilgrim', 
+        :password             => 'Ayemipape90!', 
+        :authentication       => :plain, 
+        :domain               => 'localhost.localdomain'
+      })
+
+
 
   @message = "Thanks! I'll get back to you soon!"
   erb :index
