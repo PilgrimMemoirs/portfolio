dbcc380d2c5a06e5de62657fcbf43eb7076765ea
Jamie Pilgrim <jamieleepilgrim@gmail.com>
2015-05-27 01:30:24 -0500

    added contact form

--- app.rb
+++ app.rb
@@ -2,10 +2,22 @@ require 'rubygems'
 require 'sinatra'
 require 'sinatra/activerecord'
 require './environments'
+require 'pony'
 
 class Post < ActiveRecord::Base
 end
 
 get '/' do
+  erb :index
+end
+
+
+post '/contact' do
+  name = params[:name]
+  mail = params[:mail]
+  body = params[:body]
+  Pony.mail(:to => 'hello@jamiepilgrim.com', :from => "#{mail}", :subject => "Website Inquiry from #{name}", :body => "#{body}" )
+
+  @message = "Thanks! I'll get back to you soon!"
   erb :index
 end
